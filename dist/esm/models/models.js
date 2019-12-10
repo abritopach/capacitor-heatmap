38c81ee50485a3d6c1c5f46c4d903ac3f5010ea7
abritopach <adrbrpa1988@gmail.com>
2019-12-10 23:21:26 +0100

    Added .gitignore

--- dist/esm/models/models.js
+++
@@ -1,7 +0,0 @@
-export var IHeatmapType;
-(function (IHeatmapType) {
-    IHeatmapType["Simple"] = "simple";
-    IHeatmapType["GoogleMaps"] = "googlemaps";
-    IHeatmapType["LeafletMaps"] = "leafletmaps";
-})(IHeatmapType || (IHeatmapType = {}));
-//# sourceMappingURL=models.js.map
