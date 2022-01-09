579cc174b6a4a70d15f2e0523bade75870810fc9
abritopach <adrbrpa1988@gmail.com>
2022-01-09 11:56:58 +0000

    ✨ Added mapbox map

--- examples/ionic-capacitor-heatmap/src/app/mapbox-maps-heatmap/mapbox-maps-heatmap.page.ts
+++ examples/ionic-capacitor-heatmap/src/app/mapbox-maps-heatmap/mapbox-maps-heatmap.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-mapbox-maps-heatmap',
@@ -9,9 +11,25 @@ export class MapboxMapsHeatmapPage implements OnInit {
 
   currentYear = new Date().getFullYear();
 
-  constructor() { }
+  mapbox = (mapboxgl as typeof mapboxgl);
+  map: mapboxgl.Map;
+
+  constructor() {
+    this.mapbox.accessToken = environment.MAP_BOX_API_TOKEN;
+  }
 
   ngOnInit() {
+    setTimeout(() => this.buildMap(), 1000);
+  }
+
+  buildMap() {
+    this.map = new mapboxgl.Map({
+      container: 'mapBox',
+      style: `mapbox://styles/mapbox/streets-v11`,
+      zoom: 15,
+      center: [-3.703790, 40.416775]
+    });
+    this.map.addControl(new mapboxgl.NavigationControl());
   }
 
 }
