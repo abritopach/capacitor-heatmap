26c98d730882704c0d6362b8f37fb9b9e1c440d0
abritopach <adrbrpa1988@gmail.com>
2022-01-11 19:55:20 +0000

    ⚡ Updated mapbox heatmap functionality

--- src/heatmaps/mapbox-maps-heatmap.ts
+++ src/heatmaps/mapbox-maps-heatmap.ts
@@ -6,6 +6,14 @@ import { BaseHeatmap } from './base-heatmap';
 
 export class MapboxMapsHeatmap extends BaseHeatmap {
 
+    static readonly DEFAULT_GRADIENT: HeatmapGradient = {
+        0.4: 'rgb(0, 0, 255)',
+        0.6: 'rgb(0,255,255)',
+        0.7: 'rgb(0, 255, 0)',
+        0.8: 'rgba(244, 227, 0, 1)',
+        1.0: 'rgb(178,24,43)'
+    };
+
     _map!: mapboxgl.Map;
     _canvas!: HTMLCanvasElement;
     _data: HeatmapData = [];
@@ -27,53 +35,15 @@ export class MapboxMapsHeatmap extends BaseHeatmap {
         this._map = options.map;
         console.log('map', this._map);
 
-        this._map.addSource('earthquakes', {
-            'type': 'geojson',
-            'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson'
-        });
-
-        this._map.addLayer({
-            'id': 'earthquakes-heat',
-            'type': 'heatmap',
-            'source': 'earthquakes',
-            'maxzoom': 9,
-            'paint': {
-                // Increase the heatmap weight based on frequency and property magnitude
-                'heatmap-weight': ['interpolate', ['linear'], ['get', 'mag'], 0, 0, 6, 1],
-                // Increase the heatmap color weight weight by zoom level
-                // heatmap-intensity is a multiplier on top of heatmap-weight
-                'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, 9, 3],
-                // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-                // Begin color ramp at 0-stop with a 0-transparancy color
-                // to create a blur-like effect.
-                'heatmap-color': ['interpolate', ['linear'], ['heatmap-density'], 0, 'rgba(33,102,172,0)', 0.2, 'rgb(103,169,207)', 0.4, 'rgb(209,229,240)', 0.6, 'rgb(253,219,199)', 0.8, 'rgb(239,138,98)', 1, 'rgb(178,24,43)'],
-                // Adjust the heatmap radius by zoom level
-                'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, 9, 20],
-                // Transition from heatmap to circle layer by zoom level
-                'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 7, 1, 9, 0]
-            }
-        },
-        'waterway-label'
+        this._data = typeof options.data !== 'undefined' ? options.data :
+        (
+            this._heatmapLogger.warn(`${Logs.mapbox} Data is undefined or empty. Passes heatmap data into draw function or set heatmap data with setData function.`),
+            []
         );
 
-        this._map.addLayer({
-            'id': 'earthquakes-point',
-            'type': 'circle',
-            'source': 'earthquakes',
-            'minzoom': 7,
-            'paint': {
-                // Size circle radius by earthquake magnitude and zoom level
-                'circle-radius': ['interpolate', ['linear'], ['zoom'], 7, ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4], 16, ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]],
-                // Color circle by earthquake magnitude
-                'circle-color': ['interpolate',['linear'], ['get', 'mag'], 1, 'rgba(33,102,172,0)', 2, 'rgb(103,169,207)', 3, 'rgb(209,229,240)', 4, 'rgb(253,219,199)', 5, 'rgb(239,138,98)', 6, 'rgb(178,24,43)'],
-                'circle-stroke-color': 'white',
-                'circle-stroke-width': 1,
-                // Transition from heatmap to circle layer by zoom level
-                'circle-opacity': ['interpolate', ['linear'], ['zoom'], 7, 0, 8, 1]
-            }
-        },
-        'waterway-label'
-        );
+        if (this._map) {
+            this._addHeatmapLayer2Map();
+        }
 
         return this._canvas;
 
@@ -81,6 +51,7 @@ export class MapboxMapsHeatmap extends BaseHeatmap {
 
     destroy(): void {
         this._heatmapLogger.log(`${Logs.mapbox} destroy`);
+        this._map.remove();
     }
 
 
@@ -89,13 +60,15 @@ export class MapboxMapsHeatmap extends BaseHeatmap {
     /*********/
     setData(data: HeatmapData): HeatmapData {
         this._heatmapLogger.log(`${Logs.mapbox} setData`, data);
-        // TODO: Not implemented yet.
-        return [];
+        this._data = [];
+        this._data = [...data];
+        const opt = {};
+        this.draw(opt);
+        return this._data;
     }
 
     getData(): HeatmapData {
         this._heatmapLogger.log(`${Logs.mapbox} getData`);
-                // TODO: Not implemented yet.
         return this._data;
     }
 
@@ -130,7 +103,12 @@ export class MapboxMapsHeatmap extends BaseHeatmap {
     /*********/
     draw(options: {opacity?: number, radius?: number, gradient?: string[], data?: HeatmapData}): boolean {
         this._heatmapLogger.log(`${Logs.mapbox} draw`, options);
-        // TODO: Not implemented yet.
+        if (!this._map) { return false; }
+
+        this._data = typeof options.data !== 'undefined' ? options.data : this._data;
+
+        console.log('data', this._data);
+
         return true;
     }
 
@@ -175,5 +153,60 @@ export class MapboxMapsHeatmap extends BaseHeatmap {
     /*********/
     // Private methods.
     /*********/
+
+    private _addHeatmapLayer2Map() {
+
+        this._map.on('load', () => {
+
+            this._map.addSource('earthquakes', {
+                'type': 'geojson',
+                'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson'
+            });
+
+            this._map.addLayer({
+                'id': 'earthquakes-heat',
+                'type': 'heatmap',
+                'source': 'earthquakes',
+                'maxzoom': 9,
+                'paint': {
+                    // Increase the heatmap weight based on frequency and property magnitude
+                    'heatmap-weight': ['interpolate', ['linear'], ['get', 'mag'], 0, 0, 6, 1],
+                    // Increase the heatmap color weight weight by zoom level
+                    // heatmap-intensity is a multiplier on top of heatmap-weight
+                    'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, 9, 3],
+                    // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+                    // Begin color ramp at 0-stop with a 0-transparancy color
+                    // to create a blur-like effect.
+                    'heatmap-color': ['interpolate', ['linear'], ['heatmap-density'], 0.4, 'rgb(0, 0, 255)', 0.6, 'rgb(0,255,255)', 0.7, 'rgb(0, 255, 0)', 0.8, 'rgba(244, 227, 0, 1)', 1, 'rgb(178,24,43)'],
+                    // Adjust the heatmap radius by zoom level
+                    'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, 9, 20],
+                    // Transition from heatmap to circle layer by zoom level
+                    'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 7, 1, 9, 0]
+                }
+            },
+            'waterway-label'
+            );
+
+            this._map.addLayer({
+                'id': 'earthquakes-point',
+                'type': 'circle',
+                'source': 'earthquakes',
+                'minzoom': 7,
+                'paint': {
+                    // Size circle radius by earthquake magnitude and zoom level
+                    'circle-radius': ['interpolate', ['linear'], ['zoom'], 7, ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4], 16, ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]],
+                    // Color circle by earthquake magnitude
+                    'circle-color': ['interpolate',['linear'], ['get', 'mag'], 1, 'rgba(33,102,172,0)', 2, 'rgb(103,169,207)', 3, 'rgb(209,229,240)', 4, 'rgb(253,219,199)', 5, 'rgb(239,138,98)', 6, 'rgb(178,24,43)'],
+                    'circle-stroke-color': 'white',
+                    'circle-stroke-width': 1,
+                    // Transition from heatmap to circle layer by zoom level
+                    'circle-opacity': ['interpolate', ['linear'], ['zoom'], 7, 0, 8, 1]
+                }
+            },
+            'waterway-label'
+            );
+        });
+
+    }
 
 }
